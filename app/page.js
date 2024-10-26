f197c6b0a75868069de040a5faee33b1317ac00b
MuhammedBaki99 <muhammedbakicaglayan@gmail.com>
2024-10-27 02:22:15 +0300

    comments componenti oluşturuldu

--- app/page.js
+++ app/page.js
@@ -1,10 +1,13 @@
+import Comments from "@/components/comments";
 import "./globals.css"; 
 import SuggestionsPage from "./suggestions/page";
+import EditButton from "@/components/edit-button";
 
 export default function Home() {
   return (
     <div className="container"> 
       <SuggestionsPage />
+      <EditButton />
     </div>
   );
 }
