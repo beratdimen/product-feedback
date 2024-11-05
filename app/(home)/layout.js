3bf904e17cc88ebfe1094ee7e3f34a6e4e3b45c0
Berat Dimen <beratefedimen@hotmail.com>
2024-11-06 00:37:58 +0300

    layoutlardaki html ve body kaldırıldı cünkü nextte böyle bir kullanımı next bile görmemişti

--- app/(home)/layout.js
+++ app/(home)/layout.js
@@ -1,21 +1,11 @@
 import { Toaster } from "sonner";
-import SideBar from "@/components/side-bar";
-import Header from "@/components/header";
 import { ThemeProvider } from "next-themes";
 
-export default function RootLayout({ children }) {
+export default function HomeLayout({ children }) {
   return (
-    <html lang="tr">
-      <body className={`bodycontent`}>
-        <ThemeProvider attribute="data-theme">
-          <SideBar />
-          <div>
-            <Header />
-            {children}
-          </div>
-          <Toaster richColors position="top-right" />
-        </ThemeProvider>
-      </body>
-    </html>
+    <ThemeProvider attribute="data-theme">
+      {children}
+      <Toaster richColors position="top-right" />
+    </ThemeProvider>
   );
 }
