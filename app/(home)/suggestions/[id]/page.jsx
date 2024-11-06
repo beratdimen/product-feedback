5766e5be10bc6a4c7a11ca6c6360aa8d08e5349c
MuhammedBaki99 <muhammedbakicaglayan@gmail.com>
2024-11-07 01:49:07 +0300

    kategoriler çağırılıp ekrana basıldı

--- app/(home)/suggestions/[id]/page.jsx
+++ app/(home)/suggestions/[id]/page.jsx
@@ -1,13 +1,14 @@
 "use client";
 import DetailFeedback from "@/components/detail-feedback-card";
 import Empty from "@/components/empty";
-import { getDetailFeedbacks } from "@/utils/fetchBase";
+import { getCategory, getDetailFeedbacks } from "@/utils/fetchBase";
 import { useEffect, useState } from "react";
 
 import "./style.css";
 
-export default function DetailFeedbacks({ params }) {
+export default function DetailFeedbacks({ params  }) {
   const [data, setData] = useState({});
+  const [categoryList, setcategoryList] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
       const { response, error } = await getDetailFeedbacks(params?.id);
@@ -15,10 +16,14 @@ export default function DetailFeedbacks({ params }) {
       if (!error) {
         setData(response);
       }
+
+      const responseCategory = await getCategory();
+      setcategoryList(responseCategory);
     };
 
     fetchData();
+
   }, [params?.id]);
 
-  return data ? <DetailFeedback data={data} params={params} /> : <Empty />;
+  return data ? <DetailFeedback categoryList={categoryList} data={data} params={params} /> : <Empty />;
 }
