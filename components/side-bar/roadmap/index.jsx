66bb0d70f445b2d4b93eab2a17199896696c0f87
MuhammedBaki99 <muhammedbakicaglayan@gmail.com>
2024-11-07 01:52:24 +0300

    sidebar da ki roadmap içeriğinin sayıları aktif hale getirildi ve kullanıcı bilgisi cssi, çıkış yap butonunun css i yapıldı

--- components/side-bar/roadmap/index.jsx
+++ components/side-bar/roadmap/index.jsx
@@ -1,7 +1,25 @@
- 
-import Link from "next/link";
+"use client";
 
-export default function Roadmap() {
+import Link from "next/link";
+import { useState, useEffect } from "react";
+
+export default function Roadmap({ feedbackData }) {
+  const [filterLength, setFilterLength] = useState({
+    planned: [],
+    inProgress: [],
+    live: []
+  });//burda hepsi için ayrı ayrı elemanlar oluşturuluyo
+
+  useEffect(() => {
+    if (feedbackData?.feedbacks) {
+      setFilterLength({
+        planned: feedbackData.feedbacks.filter(x => x.status === 1),
+        inProgress: feedbackData.feedbacks.filter(x => x.status === 2),
+        live: feedbackData.feedbacks.filter(x => x.status === 3)
+      });
+    }
+  }, [feedbackData]);//bu useeffect içinde ise hepsi için ayrı ayrı filtreleme yapıp çıkan sonuçlar elemanlara atanıyor.Sonrasında bu dizilerin uzunlukları gerekn yerlere yazılıyor
+
   return (
     <div className="roadmapContainer">
       <div className="roadmapHeader">
@@ -11,16 +29,16 @@ export default function Roadmap() {
       <ul>
         <li className="planned">
           <p><span>●</span> Planned</p>
-          <span>1</span>
+          <span>{filterLength.planned.length}</span>
         </li>
         <li className="inProgress">
-          <p><span>●</span>In-Progress</p>
-          <span>1</span>
-          </li>
+          <p><span>●</span> In-Progress</p>
+          <span>{filterLength.inProgress.length}</span>
+        </li>
         <li className="live">
-          <p><span>●</span>Live</p>
-          <span>1</span>
-          </li>
+          <p><span>●</span> Live</p>
+          <span>{filterLength.live.length}</span>
+        </li>
       </ul>
     </div>
   );
